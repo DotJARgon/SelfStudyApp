25e8133665046888c425db85ec25def5d2870afa
ChaserPurple10600 <ChaserPurple10600@gmail.com>
2024-02-16 19:33:08 -0600

    html layout

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 
-@app.route("/", methods=['GET'])
-def hello_world():
-    return 'hello world'
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit')
+def index():
+    return render_template('submit.html')
+@app.route('/review')
+def index():
+    return render_template('review.html')
+@app.route('/download')
+def index():
+    return render_template('download.html')
