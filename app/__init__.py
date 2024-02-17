3fe1623ee594e47166a631aef840489078b92aac
unknown <mcar3550@students.kleinisd.net>
2024-02-16 18:41:50 -0600

    routes and models

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+# from csi3335F2023 import mysql
+
+app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{mysql['user']}:{mysql['password']}@{mysql['location']}/{mysql['database']}"
+app.config["SECRET_KEY"] = "tempqwdh89hcdhxuhcix98vsd7v8cv"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
